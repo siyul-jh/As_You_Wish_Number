bbb24b74c1b7c1f5413395dd4bf9433a27bef8ad
Leejiheum <siyul@LABs-Mac.local>
2021-06-11 12:00:35 +0900

    2021-06-11 12:00, Router 미 연결 수정작업

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,9 @@
+import App from './App';
+import { Switch, Route } from 'react-router-dom';
 export default function Router() {
 	return (
-		<>
-			<Router exact path='/' />
-		</>
+		<Switch>
+			<Route exact path='/' component={App} />
+		</Switch>
 	);
 }
